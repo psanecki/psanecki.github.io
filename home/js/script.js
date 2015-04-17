f8cfa8240555f376b5536bf648b195a534c5753f
Przemek x61 <psanecki@mail.com>
2015-04-17 18:56:46 +0100

    hidden

--- home/js/script.js
+++ home/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     $('iframe').fitVids();
     $('.slideshow').slick({
         lazyLoad: 'ondemand',
@@ -13,5 +14,27 @@ $(document).ready(function(){
         // arrows: false,
         // prevArrow: '.prev',
         // nextArrow: '.next'
+    });
+    // $('#menu').sticky({topSpacing:0});
+    console.log('hello');
+    $('#menu').on('click', '.menuItem', function(evt) {
+        var id = $(evt.target).attr('href');
+        console.log('event target href = id' + id);
+
+        $(id).removeClass('tohide');
+        $(id).addClass('toshow');
+
+        var siblings = $(evt.target).parent().siblings();
+        console.log(siblings.length);
+        for(var i = 0; i < siblings.length; i++){
+            var id2 = $(siblings[i]).find('a').attr('href');
+            console.log(id2);
+
+            $(id2).addClass('tohide');
+            $(id).removeClass('toshow');
+        }
+        console.log($('.tohide'));
+        console.log($('.toshow'));
+
     });
 });
