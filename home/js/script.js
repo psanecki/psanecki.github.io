71b79e74b2e5f9d6589db869d5f295fb73133dd4
Przemek x61 <psanecki@mail.com>
2015-04-18 13:54:45 +0100

    style2 gotowe

--- home/js/script.js
+++ home/js/script.js
@@ -18,16 +18,24 @@ $(document).ready(function(){
     // $('#menu').sticky({topSpacing:0});
     console.log('hello');
     $('#menu').on('click', '.menuItem', function(evt) {
-        var id = $(evt.target).attr('href');
-        console.log('event target href = id' + id);
+        // var id = $(evt.target).attr('href');
+
+        var id = $(evt.target).data('target');
+        // console.log('event target href =');
 
         $(id).removeClass('tohide');
         $(id).addClass('toshow');
 
+        // if(id==='#works'){
+        //     $(id).css('background-color', '#000');
+        // } else {
+        //     $(id).css('background-color', 'white');
+        // }
+
         var siblings = $(evt.target).parent().siblings();
         console.log(siblings.length);
         for(var i = 0; i < siblings.length; i++){
-            var id2 = $(siblings[i]).find('a').attr('href');
+            var id2 = $(siblings[i]).find('a').data('target');
             console.log(id2);
 
             $(id2).addClass('tohide');
